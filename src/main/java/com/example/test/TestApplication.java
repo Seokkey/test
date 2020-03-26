c933d14038d13c753c501f445b817eaa9a40d1cf
yooseok <yooseok@ust21.co.kr>
2020-03-26 14:15:03 +0900

    branch 2

--- src/main/java/com/example/test/TestApplication.java
+++ src/main/java/com/example/test/TestApplication.java
@@ -8,6 +8,6 @@ public class TestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TestApplication.class, args);
-        //issue1 브랜치 커밋
+        //branch 2
     }
 }
