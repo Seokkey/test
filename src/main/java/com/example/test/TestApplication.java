8e6dd1ccc2fae3f00d5623e1e249f694b63a0c04
yooseok <yooseok@ust21.co.kr>
2020-03-26 12:52:04 +0900

    test1 여기 또 바꿔

--- src/main/java/com/example/test/TestApplication.java
+++ src/main/java/com/example/test/TestApplication.java
@@ -8,6 +8,6 @@ public class TestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TestApplication.class, args);
-        /* 충돌 테스트 여기여기 */
+        //여기 또 바꿔
     }
 }
