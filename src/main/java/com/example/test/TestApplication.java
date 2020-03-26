d2e0b206c274b04dab8ec7c9a361b78e3c0a1918
yooseok <yooseok@ust21.co.kr>
2020-03-26 12:44:17 +0900

    충돌테스트야

--- src/main/java/com/example/test/TestApplication.java
+++ src/main/java/com/example/test/TestApplication.java
@@ -8,7 +8,6 @@ public class TestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TestApplication.class, args);
-        //기존거 지우고 올리기
-        //테스트
+        /* 충돌 테스트 여기여기 */
     }
 }
