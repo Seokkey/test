e200cf5a94c8da73162bd9ff2e20aa3c3d1f6eb0
yooseok <yooseok@ust21.co.kr>
2020-09-03 15:55:24 +0900

    branch test modify

--- src/main/java/com/example/test/TestApplication.java
+++ src/main/java/com/example/test/TestApplication.java
@@ -8,6 +8,8 @@ public class TestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TestApplication.class, args);
-        //test
+        //develop 2414
+        //test1
+        //test112
     }
 }
